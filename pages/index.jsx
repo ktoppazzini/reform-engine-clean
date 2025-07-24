1465e99708da27e4029c7b668dd65ac8af7cf729
ktoppazzini <ktoppazzini@tlleanmanagement.com>
2025-07-24 15:35:49 -0400

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,9 @@
+// /pages/index.jsx
 export default function Home() {
-  return <h1>Welcome to the Reform Engine</h1>;
+  return (
+    <div>
+      <h1>Welcome to the Reform Engine</h1>
+      <p>This is the home page.</p>
+    </div>
+  );
 }
