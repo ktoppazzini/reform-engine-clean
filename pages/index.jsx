e0d3cb358204dbdd962f78f99db658fac7769beb
ktoppazzini <ktoppazzini@tlleanmanagement.com>
2025-07-26 01:46:43 +0000

    Upload latest local files

--- pages/index.jsx
+++
@@ -1,9 +0,0 @@
-// /pages/index.jsx
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to the Reform Engine</h1>
-      <p>This is the home page.</p>
-    </div>
-  );
-}
--- pages/pages.jsx
+++
@@ -1,4 +0,0 @@
-// app/page.tsx or app/page.jsx
-export default function Home() {
-  return <h1>Hello from Sovereign OPS 5</h1>;
-}
